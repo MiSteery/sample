c82b29f8ada7e9cb5b62853e8b0594fb2f7eb488
shweta <shweta.kc016@gmail.com>
2021-06-28 23:21:39 +0545

    add order button to drawer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,31 +9,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          elevation: 0.1,
-          backgroundColor: Colors.blueGrey,
-          title: Text('sample shop'),
-          actions: <Widget>[
-            new IconButton(
-                icon: Icon(
-                  Icons.shopping_cart,
-                  color: Colors.white,
-                ),
-                onPressed: () {})
+      appBar: new AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.blueGrey,
+        title: Text('sample shop'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
           ],
         ),
-        drawer: new Drawer(
-          child: new ListView(
-            children: <Widget>[
-              InkWell(
-                onTap: () {},
-                child: ListTile(
-                  title: Text('Home Page'),
-                  leading: Icon(Icons.home),
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
